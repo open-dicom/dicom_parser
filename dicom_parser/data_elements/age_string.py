a6d085f91132163734e6366e9d5daf1eecfc52ea
Zvi Baratz <z.baratz@gmail.com>
2020-06-01 13:52:56 +0300

    Fixed DecimalString and AgeString DataElement classes to return None for invalid values instead of raising exceptions.

--- dicom_parser/data_elements/age_string.py
+++ dicom_parser/data_elements/age_string.py
@@ -9,7 +9,26 @@ class AgeString(DataElement):
     # standard format of a floating point number representing years.
     N_IN_YEAR = {"Y": 1, "M": 12, "W": 52.1429, "D": 365.2422}
 
-    def parse_value(self, value) -> float:
-        duration = float(value[:-1])
-        units = value[-1]
-        return duration / self.N_IN_YEAR[units]
+    def parse_value(self, value: str) -> float:
+        """
+        Converts an Age String element's representation of age into a :obj:`float`
+        representing years.
+
+        Parameters
+        ----------
+        value : :obj:`str`
+            Age String value
+
+        Returns
+        -------
+        :obj:`float`
+            Age in years
+        """
+
+        try:
+            duration = float(value[:-1])
+            units = value[-1]
+            return duration / self.N_IN_YEAR[units]
+        # If empty or invalid value, return None
+        except ValueError:
+            pass
