8be019945e3cc37d815683cbb962c06f7cf83675
Noam Aharony <aharonyn@gmail.com>
2020-12-17 10:52:04 +0200

    Removed the last trace of parser from __init__.py file.

--- dicom_parser/__init__.py
+++ dicom_parser/__init__.py
@@ -1,5 +1,4 @@
 from dicom_parser.header import Header
 from dicom_parser.image import Image
-from dicom_parser.parser import Parser
 from dicom_parser.series import Series
 from dicom_parser.utils.read_file import read_file
