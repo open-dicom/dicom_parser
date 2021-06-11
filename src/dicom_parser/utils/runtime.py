eaadbf39a1a8fae80dc49d363b8cd0e197d34c98
Zvi Baratz <z.baratz@gmail.com>
2021-06-11 21:23:29 +0300

    Added docstrings to the runtime module.

--- src/dicom_parser/utils/runtime.py
+++ src/dicom_parser/utils/runtime.py
@@ -1,4 +1,8 @@
+"""
+Runtime information to check for conditionally supported functionality.
+"""
 import os
 import platform
 
+#: Whether the running OS is Windows or not.
 RUNNING_ON_WINDOWS = platform.system() == "Windows" or os.name == "nt"
