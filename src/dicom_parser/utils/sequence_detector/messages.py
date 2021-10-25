e7e962703d064e1f44b34d180202a1d0c621c2de
Zvi Baratz <z.baratz@gmail.com>
2021-10-25 11:57:32 +0300

    Added icontains type error message.

--- src/dicom_parser/utils/sequence_detector/messages.py
+++ src/dicom_parser/utils/sequence_detector/messages.py
@@ -2,6 +2,7 @@
 Strings and string formatting templates used in this module.
 """
 WRONG_DEFINITION_TYPE = "Sequence definition must be a dict, a list, or a tuple, not {definition_type}!"
+ICONTAINS_TYPE_ERROR = "Unable to apply icontains lookup for value-rule pairings of types {value_type}-{rule_type} respectively."
 INVALID_MODALITY = (
     "The {modality} modality has not been implemented or doesn't exist!"
 )
