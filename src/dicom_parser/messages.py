ecb8937808a8103180a4b3339aa928772d875fcb
Zvi Baratz <z.baratz@gmail.com>
2021-11-09 08:23:28 +0200

    Added missing message.

--- src/dicom_parser/messages.py
+++ src/dicom_parser/messages.py
@@ -12,6 +12,9 @@ EMPTY_SERIES_DIRECTORY: str = (
 INVALID_ELEMENT_IDENTIFIER: str = "Invalid data element identifier: {tag_or_keyword} of type {input_type}!\nData elements may only be queried using a string representing a keyword or a tuple of two strings representing a tag!"
 INVALID_INDEXING_OPERATOR: str = "Invalid indexing operator value ({key})! Must be of type str, tuple, int, or slice."
 INVALID_SERIES_DIRECTORY: str = "Series instances must be initialized with a valid directory path! Could not locate directory {path}."
-UNREGISTERED_MODALITY: str = "No sequence identifiers registered for {modality}!"
+UNREGISTERED_MODALITY: str = (
+    "No sequence identifiers registered for {modality}!"
+)
+MISSING_HEADER_INFO: str = "No header information found for {modality} sequence detection using {keys}!"
 
 # flake8: noqa: E501
