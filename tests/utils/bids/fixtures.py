31033d9ee461114c87120f04a7cd6730b3f823c9
Zvi Baratz <z.baratz@gmail.com>
2021-11-14 14:25:23 +0200

    Updated tests.

--- tests/utils/bids/fixtures.py
+++ tests/utils/bids/fixtures.py
@@ -3,7 +3,7 @@ Fixtures for the BIDS detection test case.
 """
 BIDS_PATH = {
     "mprage": {
-        "corrected": "sub-110/ses-201805011221/anat/sub-110_ses-201805011221_ce-corrected_T1w"
+        "corrected": "sub-110/ses-201805011221/anat/sub-110_ses-201805011221_acq-3_ce-corrected_T1w"
     }
 }
 
