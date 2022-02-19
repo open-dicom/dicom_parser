83ac799ff54053cabd9ee16724cf7d34e4f3337f
Zvi Baratz <z.baratz@gmail.com>
2022-02-19 17:00:33 +0200

    Fixed linting error.

--- tests/utils/siemens/csa/ascii/fixtures.py
+++ tests/utils/siemens/csa/ascii/fixtures.py
@@ -5,7 +5,7 @@ GRADSPEC.asGPAData[0].sEddyCompensationY.aflTimeConstant[1]\t = \t0.917683601379
 TXSPEC.asNucleusInfo[0].CompProtectionValues.MaxOfflineTxAmpl\t = \t534.113952637
 SliceArray.asSlice[2].sNormal.dSag\t = \t-0.01623302609
 PtabAbsStartPosZValid\t = \t0x1
-"""
+"""  # noqa: E501
 
 PARSED_ELEMENTS = {
     "GRADSPEC": {
